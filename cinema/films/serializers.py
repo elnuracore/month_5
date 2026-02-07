1a2d2d8db81aa6c1f3614f4339dfb8f663e26fa1
elnuracore <elnuraanarbaikyzy7@gmail.com>
2026-02-07 20:06:27 +0600

    month 5

---
+++ cinema/films/serializers.py
@@ -0,0 +1,44 @@
+from rest_framework import serializers
+from .models import Film, Director
+from rest_framework.error import ValidationError
+
+class DirectorSerializers(serializers.ModelSerializer):
+    class Meta:
+        model = Director
+        fields = "id fio".split()
+
+class FilmDetailSerializers(serializers.ModelSerializer):
+    class Meta:
+        model = Film
+        fields = '__all__'
+
+class FilmListSerializers(serializers.ModelSerializer):
+    director = DirectorSerializers()
+    genres = serializers.SerializerMethodField()
+    class Meta:
+        model = Film
+        fields = 'id director genres title is_hit rating created reviews'.split()
+        depth = 1
+
+    def get_genres(self, films):
+        return films.genre_names[0:2]
+
+class FilmValidateSerializers(serializers.Serializer):
+    title = serializers.CharField(min_length=1, max_length=255)
+    text = serializers.CharField(required=False)
+    release_year = serializers.IntegerField()
+    is_hit = serializers.BooleanField(default=True)
+    rating = serializers.FloatField(min_value=1, max_value=10)
+    director_id = serializers.IntegerField()
+    genres = serializers.ListField(child=serializers.IntegerField())
+
+    def validate_director_id(self, director_id):
+        try:
+            Director.objects.get(id=director_id)
+        except Director.DoesNotExist:
+            raise ValidationError("Director does not exist")
+        return director_id
+
+
+
+
---
+++ cinema/films/views.py
@@ -0,0 +1,84 @@
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+from rest_framework import status
+from .models import Film
+from django.db import transaction
+from .serializers import FilmDetailSerializers, FilmListSerializers, FilmValidateSerializers
+
+@api_view(['GET', 'PUT', 'DELETE'])
+def film_detail_api_view(request, id):
+    try:
+        film = Film.objects.get(id=id)
+    except:
+        return Response(status=status.HTTP_404_NOT_FOUND,
+                        data={'error' : 'film not found!'})
+    
+    if request.method == "GET":
+        data = FilmDetailSerializers(film, many=False).data
+        return Response(data=data)
+    
+    elif request.method == "PUT":
+
+
+        film.title = request.data.get('title')
+        film.text = request.data.get('text')
+        film.release_year = request.data.get('release_year')
+        film.is_hit = request.data.get('is_hit')
+        film.rating = request.data.get('rating')
+        film.director_id = request.data.get('director_id')  # Foreign Key field
+        film.genres.set(request.data.get('genres'))  # ManyToMany field
+        film.save()
+        return Response(status=status.HTTP_201_CREATED, 
+                        data=FilmDetailSerializers(film).data)
+    
+    elif request.method == "DELETE":
+        film.delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)
+
+
+
+
+
+
+@api_view(['GET', 'POST'])
+def film_list_api_view(request):
+    if request.method == "GET":
+        films = Film.objects.select_related('director').prefetch_related('genres', 'reviews').all()
+        data = FilmListSerializers(films, many=True).data
+
+        return Response(
+            data = data
+    )
+    elif request.method == "POST":
+        serializer = FilmValidateSerializers(data=request.data)
+        # if not serializer.is_valid():
+        #     return Response(status=status.HTTP_400_BAD_REQUEST, data=serializer.errors)
+        serializer.is_valid(raise_exception=True)
+
+        title = serializer.validated_data.get('title')
+        text = serializer.validated_data.get('text')
+        release_year = serializer.validated_data.get('release_year')
+        is_hit = serializer.validated_data.get('is_hit')
+        rating= serializer.validated_data.get('rating')
+        director_id = serializer.validated_data.get('director_id')
+        genres = serializer.validated_data.get('genres')
+        print(title, text, release_year, is_hit, rating, genres)
+
+        with transaction.atomic():
+            film  = Film.objects.create(
+                title=title,
+                text=text,
+                release_year=release_year,
+                is_hit=is_hit,
+                rating=rating,
+                director_id=director_id
+                )
+            film.genres.set(genres)
+            film.save()
+
+        return Response(status=status.HTTP_201_CREATED, data=FilmDetailSerializers(film).data)
+    
+    
+
+
+    
